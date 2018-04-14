35fc62bc359f92cc66f8d4f45c786909a2b444c0
173050023 <shivakul@cse.iitb.ac.in>
2018-04-14 18:56:53 +0530

    FinalProject-6 Create Classes

--- src/sample/Manager.java
+++ src/sample/Manager.java
@@ -4,7 +4,7 @@ public class Manager {
     int empID;
     String empName;
     int priorityLevel;
-    int cost = 0;
+    int cost;
     public Manager(int empID, String empName, int priorityLevel, int cost)
     {
         this.empID = empID;
